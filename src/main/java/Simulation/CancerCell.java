d6107725d553e90b8522c8898b5d1a77363b8f9d
maggie <maggie63@student.ubc.ca>
2023-08-31 16:45:13 -0400

    implemented CancerCell interactNeighbors method

--- src/main/java/Simulation/CancerCell.java
+++ src/main/java/Simulation/CancerCell.java
@@ -2,6 +2,8 @@ package main.java.Simulation;
 import java.util.ArrayList;
 import main.java.Util.Pair;
 
+import static main.java.Util.Calculator.coordFromIndex;
+
 /**
  *This is a cancer cell. It is the most complex cell as it can attack tissue or immune cells, or grow into a dead cell.
  * For attacking tissue, it is a 1 hit replace it with a dead cell.
@@ -22,5 +24,28 @@ public class CancerCell extends Cell{
     }
     @Override
     public void interactNeighbors(ArrayList<Cell> neighbors) {
+        int tissueCount = tissueCells.size();
+        int immuneCount = immuneCells.size();
+
+        super.interactNeighbors(neighbors);
+
+        if (!deadCells.isEmpty()) {
+            for (int index : deadCells) {
+                neighbors.set(index, new CancerCell(coordFromIndex(index)));
+            }
+        }
+        else if(tissueCount > immuneCount && tissueCount > 1) {
+            int i = (int) (Math.random() * tissueCells.size());
+            int index = tissueCells.get(i);
+            neighbors.set(index, new DeadCell(coordFromIndex(index)));
+        }
+        else {
+            int strength;
+            for (int index : immuneCells) {
+                strength = neighbors.get(index).getStrength();
+                strength = strength - 1;
+                neighbors.get(index).setStrength(strength);
+            }
+        }
     }
 }
